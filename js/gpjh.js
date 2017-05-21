bf0442b235d839dc2a575e0b6bd58eebf1fb1f0f
zhangweiza <zhangweiza@yonyou.com>
2017-05-21 12:08:02 +0800

    国品计划

---
+++ js/gpjh.js
@@ -0,0 +1,8 @@
+$(function () {
+    // $(".hy_item div[data-img]").hover(function () {
+    //     var imgSrc = $(this).data("img");
+    //     $(".img_view").show().find("img").attr("src", imgSrc)
+    // }, function () {
+    //     $(".img_view").hide();
+    // })
+})
