fccfaee00591708061f1c26f873cab9c9eb316fa
zhangweiza <zhangweiza@yonyou.com>
2017-05-21 12:47:34 +0800

    骑行圈-圈粉

--- js/service.js
+++ js/service.js
@@ -7,17 +7,4 @@ $(function () {
     $("div.dialog").on("click", "div.close", function () {
         $(this).parent().hide();
     });
-
-    if (screen.width > 1080) {
-        $("div.banner_shadow_text").each(function (index, value) {
-            var banner_text_left = $(this).width() / 2,
-                banner_text_top = $(this).height() / 2;
-            $(value).css({"marginLeft": -banner_text_left + "px", "marginTop": -banner_text_top + "px"});
-        })
-    } else {
-        $("div.banner_shadow_text").each(function (index, value) {
-            var banner_text_top = $(this).height() / 2;
-            $(value).css({"marginTop": -banner_text_top + "px"});
-        })
-    }
 })
