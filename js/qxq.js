17ad8cb2145c84b034511c169251327205f3ea0f
zhangweiza <zhangweiza@yonyou.com>
2017-05-28 21:50:32 +0800

    页面调整

--- js/qxq.js
+++ js/qxq.js
@@ -3,4 +3,17 @@ $(function () {
     $(".time-horizontal").children("li").each(function (i, v) {
         $(v).width(Math.floor(timeLineWidth));
     })
+
+    if (screen.width > 1080) {
+        $("div.banner_shadow_text").each(function (index, value) {
+            var banner_text_left = $(this).width() / 2,
+                banner_text_top = $(this).height() / 2;
+            $(value).css({"marginLeft": -banner_text_left + "px", "marginTop": -banner_text_top + "px"});
+        })
+    } else {
+        $("div.banner_shadow_text").each(function (index, value) {
+            var banner_text_top = $(this).height() / 2;
+            $(value).css({"marginTop": -banner_text_top + "px"});
+        })
+    }
 })
